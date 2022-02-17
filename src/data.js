363f855540b1af80f4da9e5ec417e37e6de125b5
Dan13l4 <kdbaron426@gmail.com>
2022-02-17 11:55:32 -0500

    make a update with the data.js

--- src/data.js
+++ src/data.js
@@ -1,4 +1,17 @@
+//FUNCIONES DE FILTRO
 export const filterData = (data, condition) => {
+  if (condition === "") {
+    const arrFiltered = data.filter(eachChampion => eachChampion);  
+    return arrFiltered;
+  }
   const arrFiltered = data.filter(eachChampion => eachChampion.tags.includes(condition));
   return arrFiltered;
 }; 
+
+
+//FUNCION DE BUSQUEDA
+
+export const searchInput = (data, value) => {
+  const search = data.filter(eachChampion => eachChampion.name.toLowerCase().includes(value.toLowerCase()));
+  return search;
+}
