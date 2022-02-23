561a5e3085c927017b1b88b5ab2df03b52d97e63
Dan13l4 <kdbaron426@gmail.com>
2022-02-23 17:03:49 -0500

    making upgrade

--- src/main.js
+++ src/main.js
@@ -10,118 +10,28 @@ const champs = data;
 const champions = champs.data;
 let Filteredchampions = champs.data;
 
-//FILTRO ASESINO
-document.getElementById("Assassin").addEventListener("click", ()=>{
-  const condition = "Assassin";
-  Filteredchampions = (filterData(Object.values(champions), condition));
-  document.getElementById("todos").innerHTML = '';
-  for(let i = 0; i < Filteredchampions.length; i++){ 
-      document.getElementById("todos").innerHTML +=  `<div class="cards asesinodos"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
-    }
-//MODAL EN FILTRO ASESINO
-    const cards = document.getElementsByClassName("asesinodos");
-        for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-        document.getElementById("modal").style.display= "flex";
-        document.getElementById("segundaseccion").innerHTML = `<h1>${Filteredchampions[i].name}</h1> <h2>${Filteredchampions[i].title}</h2> <img src = "${Filteredchampions[i].splash}" width = 500> <p>${Filteredchampions[i].blurb}</p>`
-        console.log(Filteredchampions[i]);
-    })
-  }
-});
-  
-//FILTRO MAGO
-document.getElementById("Mage").addEventListener("click", ()=>{
-  const condition = "Mage";
-  Filteredchampions = (filterData(Object.values(champions), condition));
-  document.getElementById("todos").innerHTML = '';
-  for(let i = 0; i < Filteredchampions.length; i++){
-      document.getElementById("todos").innerHTML +=  `<div class="cards magodos"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
-    }
+//Intento de filtro por todos 
 
-//MODAL EN FILTRO MAGE
-  const cards = document.getElementsByClassName("magodos");
+let userSelection = document.getElementsByClassName("roleOption");
+
+for(let i = 0; i < userSelection.length; i++){
+  userSelection[i].addEventListener("click", () =>{
+    let condition = document.getElementById(userSelection[i].id).value;
+    Filteredchampions = (filterData(Object.values(champions), condition));
+    document.getElementById("todos").innerHTML = '';
+    for(let i = 0; i < Filteredchampions.length; i++){
+      document.getElementById("todos").innerHTML +=  `<div class="cards"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
+    };
+    const cards = document.getElementsByClassName("cards");
       for (let i = 0; i < cards.length; i++) {
-      cards[i].addEventListener("click", () => {
-        document.getElementById("modal").style.display= "flex";
-        document.getElementById("segundaseccion").innerHTML = `<h1>${Filteredchampions[i].name}</h1> <h2>${Filteredchampions[i].title}</h2> <img src = "${Filteredchampions[i].splash}" width = 500> <p>${Filteredchampions[i].blurb}</p>`
-    })
-  }
-});
-
-//FILTRO FIGHTER
-document.getElementById("Fighter").addEventListener("click", ()=>{
-  const condition = "Fighter";
-  Filteredchampions = (filterData(Object.values(champions), condition));
-  document.getElementById("todos").innerHTML = '';
-  for(let i = 0; i < Filteredchampions.length; i++){
-      document.getElementById("todos").innerHTML +=  `<div class="cards peladordos"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
-    }
-//MODAL EN FILTRO FIGHTER
-    const cards = document.getElementsByClassName("peladordos");
-       for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
+          cards[i].addEventListener("click", () => {
           document.getElementById("modal").style.display= "flex";
           document.getElementById("segundaseccion").innerHTML = `<h1>${Filteredchampions[i].name}</h1> <h2>${Filteredchampions[i].title}</h2> <img src = "${Filteredchampions[i].splash}" width = 500> <p>${Filteredchampions[i].blurb}</p>`
-        console.log(Filteredchampions[i]);
-    })
-  }
-});
+      });
+    };
+  });
+};
 
-//FILTRO MARKSMAN
-document.getElementById("Marksman").addEventListener("click", ()=>{
-  const condition = "Marksman";
-  Filteredchampions = (filterData(Object.values(champions), condition));
-  document.getElementById("todos").innerHTML = '';
-  for(let i = 0; i < Filteredchampions.length; i++){
-      document.getElementById("todos").innerHTML +=  `<div class="cards tiradordos"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
-    }
-//MODAL EN FILTRO MARKSMAN
-    const cards = document.getElementsByClassName("tiradordos");
-       for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-          document.getElementById("modal").style.display= "flex";
-          document.getElementById("segundaseccion").innerHTML = `<h1>${Filteredchampions[i].name}</h1> <h2>${Filteredchampions[i].title}</h2> <img src = "${Filteredchampions[i].splash}" width = 500> <p>${Filteredchampions[i].blurb}</p>`
-        console.log(Filteredchampions[i]);
-    })
-  }
-});
-
-//FILTRO SUPPORT
-document.getElementById("Support").addEventListener("click", ()=>{
-  const condition = "Support";
-  Filteredchampions = (filterData(Object.values(champions), condition));
-  document.getElementById("todos").innerHTML = '';
-  for(let i = 0; i < Filteredchampions.length; i++){
-      document.getElementById("todos").innerHTML +=  `<div class="cards soportedos"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
-    }
-//MODAL EN FILTRO SUPPORT
-    const cards = document.getElementsByClassName("soportedos");
-        for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-          document.getElementById("modal").style.display= "flex";
-          document.getElementById("segundaseccion").innerHTML = `<h1>${Filteredchampions[i].name}</h1> <h2>${Filteredchampions[i].title}</h2> <img src = "${Filteredchampions[i].splash}" width = 500> <p>${Filteredchampions[i].blurb}</p>`
-        console.log(Filteredchampions[i]);
-    })
-  }
-});
-
-//FILTRO TANK
-document.getElementById("Tank").addEventListener("click", ()=>{
-  const condition = "Tank";
-  Filteredchampions = (filterData(Object.values(champions), condition));
-  document.getElementById("todos").innerHTML = '';
-  for(let i = 0; i < Filteredchampions.length; i++){
-      document.getElementById("todos").innerHTML +=  `<div class="cards tanquedos"><img src = "${Filteredchampions[i].splash}" width = 400> <p> ${Filteredchampions[i].name}</p></div>`;
-    }
-//MODAL EN FILTRO TANK
-    const cards = document.getElementsByClassName("tanquedos");
-        for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-          document.getElementById("modal").style.display= "flex";
-          document.getElementById("segundaseccion").innerHTML = `<h1>${Filteredchampions[i].name}</h1> <h2>${Filteredchampions[i].title}</h2> <img src = "${Filteredchampions[i].splash}" width = 500> <p>${Filteredchampions[i].blurb}</p>`
-    })
-  }
-});
 
 //filtro todos
 document.getElementById("All").addEventListener("click", ()=>{
