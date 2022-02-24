c755634d64b17bc4adf19066c3fd5b9fe7d1f47e
Dan13l4 <kdbaron426@gmail.com>
2022-02-24 09:10:50 -0500

    making a update in data js

--- src/data.js
+++ src/data.js
@@ -12,6 +12,29 @@ export const filterData = (data, condition) => {
 //FUNCION DE BUSQUEDA
 
 export const searchInput = (data, value) => {
+  if(value === ""){
+    return data;
+  }
   const search = data.filter(eachChampion => eachChampion.name.toLowerCase().includes(value.toLowerCase()));
   return search;
-}
+};
+
+//CALCULAR PROMEDIO
+
+export const average =(eachChampion) => {
+  let sumAttack = 0;
+  for (let i = 0; i < eachChampion.length; i++){
+    sumAttack += eachChampion[i].stats.attackdamageperlevel;
+  };
+  return sumAttack / eachChampion.length;
+};
+
+// FUNCION CALCULAR
+export const computeStats = (arr, num, valor) => {
+  if (valor === 'higher') {
+    const higherPerLevel = arr.filter(eachChampion => eachChampion.stats.attackdamageperlevel > num);
+    return higherPerLevel;
+  }
+  const lessPerLevel = arr.filter(eachChampion => eachChampion.stats.attackdamageperlevel < num);
+  return lessPerLevel;
+};
